7be84baf1c790dbdd7a50709083b01cac0b59440
Rezu0 <yunianrezky031@gmail.com>
2024-03-25 21:54:11 +0700

    add fitur checking token exp

--- src/users/handler-users.js
+++ src/users/handler-users.js
@@ -6,6 +6,7 @@ const {
   handlerGetToken,
   handlerGetFullProfileMAL,
 } = require('../utils/handler-axios');
+const { handlerUserByNameMAL } = require('../model/model-users');
 
 const handlerCallbackFromMal = async (request, h) => {
   const { code } = request.payload;
@@ -36,17 +37,33 @@ const handlerCallbackFromMal = async (request, h) => {
 const checkingTokenExp = async (request, h) => {
   const credentialsUser = request.auth.credentials;
   const tokenUser = request.headers.authorization;
+  const tokenSplit = tokenUser.split(' ');
 
-  const response = h.response({
-    status: 'success',
-    message: 'Test Token',
-    data: {
-      credentialsUser,
-      tokenUser,
-    },
-  });
-  response.code(200);
-  return response;
+  try {
+    const userFind = await handlerUserByNameMAL(credentialsUser.name_mal);
+    const currentTime = Math.floor(Date.now() / 1000);
+    if (userFind.token !== tokenSplit[1]) {
+      return h.response({
+        status: 'fail',
+        message: 'Credentials tidak valid!',
+      }).code(401);
+    }
+
+    if (credentialsUser.exp < currentTime) {
+      return h.response({
+        status: 'fail',
+        message: 'Token Expired',
+      }).code(401);
+    }
+
+    return h.response({
+      status: 'success',
+      message: 'Token tidak expired!',
+      data: credentialsUser,
+    }).code(200);
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 module.exports = {
