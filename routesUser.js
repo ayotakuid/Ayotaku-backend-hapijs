f27382abd976d58c8461243ba7a1b90a3a4f3868
Rezu0 <yunianrezky031@gmail.com>
2024-07-24 19:40:12 +0700

    delete route user/api dan debugging beberapa request

--- routesUser.js
+++ routesUser.js
@@ -1,16 +1,54 @@
+const handlerErrorLimitRoutes = require("./src/error/handler-error-limit");
 const { createTokenUsers } = require("./src/utils/handler-token");
 
 const routesUser = [
   {
+    method: ['POST', 'GET'],
+    path: '/user/auth/google',
+    handler: async (request, h) => {
+      const { profile } = request.auth.credentials;
+      try {
+        const dataToken = JSON.stringify(profile.raw);
+        return h.redirect(`/user/api/test?raw=${dataToken}`);
+      } catch (err) {
+        console.error('Terjadi error saat login google: ', err);
+        return h.response({
+          status: 'error',
+          message: 'Terjadi kesalahan, Coba lagi.',
+        }).code(500);
+      }
+    },
+    options: {
+      auth: 'google',
+      cors: true,
+      plugins: {
+        'hapi-rate-limit': {
+          pathLimit: 1,
+          pathCache: {
+            expiresIn: 10000,
+          },
+          authCache: {
+            expiresIn: 5000,
+          },
+        },
+      },
+    },
+  },
+  {
     method: 'GET',
-    path: '/user/api',
-    handler: (request, h) => {
-      const authenticated = request.auth;
+    path: '/user/api/test',
+    handler: async (request, h) => {
+      const paramRaw = request.query.raw;
       return h.response({
         status: 'success',
-        message: 'test',
-        data: authenticated,
+        data: JSON.parse(paramRaw),
       }).code(200);
+      // const credentialsUser = request.auth.credentials;
+      // return h.response({
+      //   status: 'berhasil',
+      //   message: 'Damn man',
+      //   data: credentialsUser,
+      // }).code(200);
     },
     options: {
       auth: false,
@@ -18,42 +56,22 @@ const routesUser = [
     },
   },
   {
-    method: ['POST', 'GET'],
-    path: '/auth/google',
-    handler: async (request, h) => {
-      const { profile } = request.auth.credentials;
-      const dataToken = profile.raw;
-      const createToken = createTokenUsers(dataToken);
-      return h.response({
-        status: 'success',
-        data: createToken,
-      }).code(200);
-    },
-    options: {
-      auth: 'google',
-      cors: true,
-    },
-  },
-  {
-    method: 'GET',
-    path: '/user/api/test',
-    handler: async (request, h) => {
-      const credentialsUser = request.auth.credentials;
-      return h.response({
-        status: 'berhasil',
-        message: 'Damn man',
-        data: credentialsUser,
-      }).code(200);
-    },
-    options: {
-      auth: 'jwtUsers',
-      cors: true,
-    },
-  },
-  {
     method: 'GET',
     path: '/coba',
-    handler: async (request, h) => h.response({ status: 'success' }).code(200),
+    handler: async (request, h) => {
+      const { response } = request;
+      try {
+        return h.response({
+          status: 'success',
+        }).code(200);
+      } catch (err) {
+        console.error('Terjadi error saat login google: ', err);
+        return h.response({
+          status: 'error',
+          message: 'Terjadi kesalahan, Coba lagi.',
+        }).code(500);
+      }
+    },
     options: {
       auth: false,
       cors: true,
