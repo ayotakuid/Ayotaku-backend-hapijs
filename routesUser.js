d2bc41e802eb966f8d56e3a389b63d1bcd59c6d1
Rezu0 <yunianrezky031@gmail.com>
2024-07-26 21:43:29 +0700

    fixing handler, plugins dan auth untuk routes users

--- routesUser.js
+++ routesUser.js
@@ -1,3 +1,4 @@
+const { handlerGoogleLoginUsers, handlerCallbackAfterLoginGoogle } = require("./src/ayotaku-users/handler-users/handler-users");
 const handlerErrorLimitRoutes = require("./src/error/handler-error-limit");
 const { createTokenUsers } = require("./src/utils/handler-token");
 
@@ -5,19 +6,7 @@ const routesUser = [
   {
     method: ['POST', 'GET'],
     path: '/user/auth/google',
-    handler: async (request, h) => {
-      const { profile } = request.auth.credentials;
-      try {
-        const dataToken = JSON.stringify(profile.raw);
-        return h.redirect(`/user/api/test?raw=${dataToken}`);
-      } catch (err) {
-        console.error('Terjadi error saat login google: ', err);
-        return h.response({
-          status: 'error',
-          message: 'Terjadi kesalahan, Coba lagi.',
-        }).code(500);
-      }
-    },
+    handler: handlerGoogleLoginUsers,
     options: {
       auth: 'google',
       cors: true,
@@ -36,44 +25,36 @@ const routesUser = [
   },
   {
     method: 'GET',
-    path: '/user/api/test',
-    handler: async (request, h) => {
-      const paramRaw = request.query.raw;
-      return h.response({
-        status: 'success',
-        data: JSON.parse(paramRaw),
-      }).code(200);
-      // const credentialsUser = request.auth.credentials;
-      // return h.response({
-      //   status: 'berhasil',
-      //   message: 'Damn man',
-      //   data: credentialsUser,
-      // }).code(200);
-    },
+    path: '/user/api/google/callback',
+    handler: handlerCallbackAfterLoginGoogle,
     options: {
       auth: false,
       cors: true,
+      plugins: {
+        'hapi-rate-limit': {
+          pathLimit: 1,
+          pathCache: {
+            expiresIn: 10000,
+          },
+          authCache: {
+            expiresIn: 5000,
+          },
+        },
+      },
     },
   },
   {
     method: 'GET',
-    path: '/coba',
+    path: '/user/api/profile',
     handler: async (request, h) => {
-      const { response } = request;
-      try {
-        return h.response({
-          status: 'success',
-        }).code(200);
-      } catch (err) {
-        console.error('Terjadi error saat login google: ', err);
-        return h.response({
-          status: 'error',
-          message: 'Terjadi kesalahan, Coba lagi.',
-        }).code(500);
-      }
+      const credentialsUser = request.auth.credentials;
+      return h.response({
+        status: 'success',
+        data: credentialsUser,
+      }).code(200);
     },
     options: {
-      auth: false,
+      auth: 'jwtUsers',
       cors: true,
       plugins: {
         'hapi-rate-limit': {
