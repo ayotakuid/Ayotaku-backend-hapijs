6004696518301cf8f412753f97fd130c06e497fc
Rezu0 <yunianrezky031@gmail.com>
2024-08-11 18:33:14 +0700

    add model baru untuk update info login dan model activated account

--- src/ayotaku-model-users/ayotaku-model-users.js
+++ src/ayotaku-model-users/ayotaku-model-users.js
@@ -4,6 +4,20 @@ const { client, DB_NAME } = require('../db/config');
 const db = client.db(DB_NAME);
 const collection = db.collection('ayotaku_not_admin');
 
+const modelFindUserGlobal = async (email) => {
+  const query = {
+    "from_google.email": email,
+  };
+
+  try {
+    const findUserGlobal = await collection.findOne(query);
+    return findUserGlobal;
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
+};
+
 const modelSaveUserInformation = async (newUser, tokenCreatedWeb, tokenCreatedMob) => {
   const fieldInformationUser = {
     uuid: nanoid(16),
@@ -35,14 +49,76 @@ const modelSaveUserInformation = async (newUser, tokenCreatedWeb, tokenCreatedMo
         status: 'success',
         message: 'data sudah ada',
         register: false,
+        user: findUser,
       };
     }
 
     const result = await collection.insertOne(fieldInformationUser);
+    const findAfterInsert = await collection.findOne({ "from_google.email": newUser.parseDataRaw.email });
     return {
       status: 'success',
       message: 'data tidak ada',
       register: true,
+      user: findAfterInsert,
+    };
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
+};
+
+const modelUpdateUserInfoLogin = async (email, createTokenAccess) => {
+  const query = {
+    "from_google.email": email,
+  };
+
+  try {
+    const checkingEmailStatus = await collection.findOne(query);
+    if (!checkingEmailStatus.account_active) {
+      return {
+        status: 'fail',
+        message: 'Account belom aktif',
+        account: false,
+      };
+    }
+    const dataUpdate = {
+      $set: {
+        isLogin: true,
+        timeLogin: new Date().toISOString(),
+        tokenWeb: createTokenAccess,
+      },
+    };
+    const updateUserLogin = await collection.updateOne(query, dataUpdate);
+    const findUser = await collection.findOne(query);
+    return findUser;
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
+};
+
+const modelActivatedAccount = async (userInformation) => {
+  const query = {
+    "from_google.email": userInformation.email,
+  };
+
+  try {
+    const findUser = await collection.findOne(query);
+    if (findUser.code_actived !== userInformation.code) {
+      return {
+        status: 'fail',
+        message: 'Kode aktifasi tidak cocok!',
+      };
+    }
+
+    const updatingValidCode = await collection.updateOne(query, {
+      $set: {
+        account_active: true,
+      },
+    });
+    return {
+      status: 'success',
+      message: 'Account berhasil di aktifkan!',
     };
   } catch (err) {
     console.error(err);
@@ -51,5 +127,8 @@ const modelSaveUserInformation = async (newUser, tokenCreatedWeb, tokenCreatedMo
 };
 
 module.exports = {
+  modelFindUserGlobal,
   modelSaveUserInformation,
+  modelUpdateUserInfoLogin,
+  modelActivatedAccount,
 };
