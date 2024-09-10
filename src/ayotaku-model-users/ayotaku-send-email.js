b796d666a548d04aac4b343b1cd96f4e33b45f93
Rezu0 <yunianrezky031@gmail.com>
2024-09-10 13:42:01 +0700

    add: template baru untuk kirim email change password ke user

--- src/ayotaku-model-users/ayotaku-send-email.js
+++ src/ayotaku-model-users/ayotaku-send-email.js
@@ -74,6 +74,81 @@ const sendCodeVerifyUser = async (emailUser, code) => {
   });
 };
 
+const sendLinkChangePassword = async (emailUser, data) => {
+  const transporterEmail = nodeMailer.createTransport({
+    host: HOST_TRANSPORT_EMAIL,
+    port: 465,
+    secure: true,
+    auth: {
+      user: USER_EMAIL_SUPPORT,
+      pass: PASSWORD_EMAIL_SUPPORT,
+    },
+    debug: true,
+    logger: true,
+  });
+
+  const mailOptions = {
+    from: '"Ayotaku.id Support" <support@stagingadmin.my.id>',
+    to: emailUser,
+    subject: '[no-reply] Reset Password',
+    html: `<!doctype html>
+      <html lang="en">
+        <head>
+          <meta charset="utf-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1">
+          <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        </head>
+        <body>
+          <div class="container">
+            <div class="row">
+              <div class="col-md-12">
+                <div class="card mt-2" style="width: 70%;">
+                  <div class="card-body">
+                    <h5 class="card-title">Reset Password</h5>
+                    <p class="card-text" style="font-size: 13px;">
+                      Ini adalah Email Otomatis yang dikirim kan kepada User untuk memberikan Link Reset Password, Click button di bawah ini untuk masuk ke Form Reset Password Account Ayotaku.id! <strong>Link hanya berlaku selama 2 Menit.</strong>
+                    </p>
+                    <a class="btn btn-primary btn-sm" id="click">Reset Password</a>
+
+                    <p class="card-text mt-4" style="font-size: 13px;">Atau kalian bisa Click URL dibawah ini jika Button tidak mengarahkan Anda ke Form Reset Password.</p>
+                    <a 
+                      href="http://localhost:5173/profile/me/password?email=asd&code=asd&exp=asd" 
+                      style="font-size: 13px;"
+                    >
+                      http://localhost:5173/profile/me/password?email=asd&code=asd&exp=asd
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <script>
+            document.getElementById('click').onclick = () => {
+              const url = 'http://localhost:5173/profile/me/password?email=asd&code=asd&exp=asd'
+              window.open(url, "_blank");
+            }
+          </script>
+          <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+        </body>
+      </html>`,
+  };
+
+  transporterEmail.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      return {
+        status: 'fail',
+      };
+    }
+
+    return {
+      status: 'success',
+      data: info.messageId,
+    };
+  });
+};
+
 module.exports = {
   sendCodeVerifyUser,
+  sendLinkChangePassword,
 };
