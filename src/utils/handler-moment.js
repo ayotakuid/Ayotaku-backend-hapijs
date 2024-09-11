ed29407f775b55b8baa737a0bdd893102ea135cd
Rezu0 <yunianrezky031@gmail.com>
2024-09-11 14:11:04 +0700

    add: create & validate untuk ticket code reset password selama 2 menit

---
+++ src/utils/handler-moment.js
@@ -0,0 +1,51 @@
+const moment = require('moment');
+const { nanoid } = require('nanoid');
+const { modelFindTicketResetPassword } = require('../ayotaku-model-users/ayotaku-model-password');
+
+const createTicketCodeReset = (userId) => {
+  const code = nanoid(64);
+  const exp = moment().add(2, 'minutes');
+
+  return { userId, code, exp };
+};
+
+const validateCode = async (userId, ticketCode) => {
+  try {
+    const findTicket = await modelFindTicketResetPassword(ticketCode);
+
+    if (!findTicket.ticket) {
+      return {
+        validate: false,
+        message: 'Ticket tidak ditemukan!',
+      };
+    }
+
+    if (findTicket?.data.userId !== userId && findTicket?.data.code === ticketCode) {
+      return {
+        validate: false,
+        message: 'Ticket/User tidak cocok!',
+      };
+    }
+
+    const isExpired = moment().isAfter(findTicket?.data.exp);
+    if (isExpired) {
+      return {
+        validate: false,
+        message: 'Code telah expired!',
+      };
+    }
+
+    return {
+      validate: true,
+      message: 'Code still valid',
+    };
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
+};
+
+module.exports = {
+  createTicketCodeReset,
+  validateCode,
+};
