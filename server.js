3a594ef04c960bec7039a2fe0312872fdf32f1a9
Rezu0 <yunianrezky031@gmail.com>
2024-10-05 13:45:01 +0700

    add: add origin baru

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ const init = async () => {
     host: 'localhost',
     routes: {
       cors: {
-        origin: ['http://127.0.0.1:8000', 'http://localhost:5173'],
+        origin: ['http://127.0.0.1:8000', 'http://localhost:5173', 'http://localhost:5174'],
         credentials: true,
       },
     },
